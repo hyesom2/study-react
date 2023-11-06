bf067ca99d93afed15d82064265642c4ca851c56
kimhyesom <kimhyesom1202@gmail.com>
2023-11-06 13:14:58 +0900

    🚚 Move the Main, Poster1 component from header component to App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+// > components
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import Poster1 from './components/Main/Poster/Poster1';
 
 const App = () => {
   return (
     <>
       <Header />
+      <Main>
+        <Poster1 />
+      </Main>
     </>
   );
 }
