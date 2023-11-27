af16c5713ba593ddfe49860c232de9938c7c9d0d
kimhyesom <kimhyesom1202@gmail.com>
2023-11-27 16:34:33 +0900

    💄 Markup & Style the footer component (mobile.ver)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,36 +1,143 @@
 import React from 'react';
 import styled from 'styled-components';
+import IconArrowUp from '../assets/icons/icon_arrow_up.svg';
+import IconArrowRight from '../assets/icons/icon_arrow_right_white.svg';
 
 const Container = styled.footer`
-
+  display: flex;
+  flex-direction: column;
 `;
 
 const FooterFeedback = styled.div`
+  background-color: #eceff1;
+  padding: 20px;
 
-`;
+  h1 {
+    color: #000;
+    font-size: 18px;
+    font-weight: 700;
+    letter-spacing: 1px;
+    line-height: 24px;
+    text-transform: uppercase;
+    margin-bottom: 15px;
+  }
 
-const ToTopButton = styled.button`
+  p, .feedback-link {
+    color: #000;
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0;
+    line-height: 22px;
+  }
 
-`;
-
-const FooterMenu = styled.ul`
-  display: flex;
-  flex-wrap: wrap;
-  background-color: #000;
-  
-  li {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-    padding: 10px 15px;
-    color: #fff;
+  .feedback-link {
+    margin-top: 15px;
+    text-decoration: underline;
   }
 `;
 
-const FooterAccount = styled.div``;
+const ToTopButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
 
-const FooterMembership = styled.div``;
+  img {
+    display: block;
+    width: 16px;
+    height: 16px;
+    margin-right: 10px;
+  }
+
+  span {
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 0;
+    line-height: 20px;
+  }
+`;
+
+const FooterAccount = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 15px;
+  background-color: #000;
+
+  a {
+    color: #fff;
+    font-size: 14px;
+    font-weight: 700;
+    letter-spacing: 0;
+    line-height: 20px;
+    text-transform: uppercase;
+  }
+`;
+
+const FooterMembership = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #ede734;
+  padding: 40px;
+  text-align: center;
+
+  h3 {
+    font-size: 30px;
+    font-weight: 700;
+    line-height: 32px;
+    text-transform: uppercase;
+    margin-bottom: 15px;
+  }
+  
+  p {
+    font-size: 16px;
+    font-weight: 400;
+    letter-spacing: 0;
+    line-height: 22px;
+    margin-bottom: 15px;
+  }
+
+  button {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #000;
+    height: 50px;
+    padding: 0 15px;
+    transition: all 0.3s ease-in-out;
+
+    span {
+      color: #fff;
+      font-size: 16px;
+      font-weight: 700;
+      letter-spacing: 2px;
+      lien-height: 20px;
+    }
+
+    img {
+      display: block;
+      width: 40px;
+      height: 30px;
+    }
+
+    &:hover {
+      opacity: 0.7;
+    }
+
+    &::after {
+      position: absolute;
+      top: 2px;
+      left: 2px;
+      content: '';
+      width: 142px;
+      height: 50px;
+      border: 1px solid #000;
+    }
+  }
+`;
 
 const Etc = styled.ul`
   padding: 20px 15px;
@@ -104,47 +211,38 @@ const FooterBottom = styled.div`
 const Footer = () => {
   return (
     <>
-      <Container>
+      <Container id="footer">
         <FooterFeedback>
-          <h3>모두의 의견에 귀를 기울입니다</h3>
-          <p>피드백 감사드립니다. 더 나은 서비스를 위해 노력하겠습니다. </p>
+          <h1>모두의 의견에 귀를 기울입니다</h1>
+          <p>
+            피드백 감사드립니다. 더 나은 서비스를 위해 노력하겠습니다.
+          </p>
+          <div className="feedback-link" aria-label="설문 링크">
+            <a href="/">
+              금방 끝납니다. 설문에 참여해주세요
+            </a>
+          </div>
         </FooterFeedback>
-        <ToTopButton type="button">
-          BACK TO TOP
+        <ToTopButton type="button" aria-label="위로가기 버튼">
+          <img src={ IconArrowUp } alt="" />
+          <span>
+            BACK TO TOP
+          </span>
         </ToTopButton>
         <FooterAccount>
-          <a href="/">LOGIN</a>
-          <a href="/">장바구니(0)</a>
+          <a href="/" aria-label="로그인 페이지 링크">LOGIN</a>
+          <a href="/" aria-label="장바구니 페이지 링크">장바구니 (<span>0</span>)</a>
         </FooterAccount>
         <FooterMembership>
           <h3>지금 멤버십 가입하고 10% 할인 혜택 받으세요.</h3>
           <p>* 이메일 수신동의자에 한해 10% 할인쿠폰 발급</p>
-          <button type="button">
+          <button type="button" aria-label="회원가입 버튼">
             <span>
               회원가입
             </span>
+            <img src={ IconArrowRight } alt="이동하기 버튼" aria-hidden />
           </button>
         </FooterMembership>
-        <FooterMenu>
-          <li>
-            <a href="/">배송</a>
-          </li>
-          <li>
-            <a href="/">반품</a>
-          </li>
-          <li>
-            <a href="/">주문조회</a>
-          </li>
-          <li>
-            <a href="/">구매 이용약관</a>
-          </li>
-          <li>
-            <a href="/">도움말</a>
-          </li>
-          <li>
-            <a href="/">채용정보</a>
-          </li>
-        </FooterMenu>
         <Etc>
           <li>아디다스코리아(유)</li>
           <li>서울특별시 서초구 서초대로 74길 4, 삼성생명 서초타워 23층 (06620)</li>
@@ -180,10 +278,10 @@ const Footer = () => {
               <a href="/">채무지급보증안내</a>
             </li>
           </ul>
-          <div className="copyright">
+          <div className="copyright" aria-label="저작권">
             <span>ⓒ adidas Korea, LLC. All Rights Reserved</span>
           </div>
-        </FooterBottom>  
+        </FooterBottom>
       </Container>
     </>
   );
