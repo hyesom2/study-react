f34b25746a661d2fdb4ddbd6e092a0b4c8a63c77
kimhyesom <kimhyesom1202@gmail.com>
2023-12-08 17:14:44 +0900

    ⚡️ Edit markup of banner-modal & Add banner's movement

--- src/components/Header/Banner.js
+++ src/components/Header/Banner.js
@@ -2,7 +2,7 @@ import { React, useEffect, useState } from 'react';
 import styled, { keyframes } from 'styled-components';
 // > icons
 import { ReactComponent as IconMoreDown } from '../../assets/icons/icon_more_down.svg';
-// import IconCloseButton from '../../assets/icons/icon_button_close.svg';
+import { ReactComponent as IconCloseButton } from '../../assets/icons/icon_button_close.svg';
 // > data
 import BannerData from '../../assets/data/BannerData';
 
@@ -75,162 +75,132 @@ const BannerWrapper = styled.div`
   }
 `;
 
-// const openDropdown = keyframes`
-//   0% {
-//     height: 0;
-//   }
-//   100% {
-//     height: 100%;
-//   }
-// `;
+const BannerModal = styled.section`
+  z-index: 1000;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.colors.white};
+  transform: translateY(-100%);
+  transition: transform 0.3s ease-in-out;
 
-// const BannerModal = styled.section`
-//   z-index: 1000;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-//   align-items: flex-start;
-//   width: 100%;
-//   height: 100%;
-//   background-color: #fff;
+  &.modal-open {
+    transform: translateY(0);
+  }
 
-//   &.banner-open {
-//     display: block;
-//     animation: ${openDropdown} 0.5s ease-in-out;
-//   }
-  
-//   &.banner-close {
-//     display: none;
-//   }
+  @media (min-width: 600px) {}
+  @media (min-width: 960px) {
+    height: auto;
+    max-height: 500px;
+  }
 
-//   .banner-modal-close-button-wrapper {
-//     display: flex;
-//     justify-content: flex-end;
-//     width: 100%;
-//     padding: 20px 20px 0 0;
-//   }
-
-//   .banner-modal-close-button {
-//     width: 50px;
-//     height: 50px;
-//     border: 1px solid #000;
+  .banner-modal-close-button {
+    display: flex;
+    justify-content: flex-end;
+    padding: 20px 20px 0 0;
     
-//     img {
-//       display: block;
-//       width: 100%;
-//       height: 100%;
-//       padding: 10px;
-//     }
-//   }
-  
-//   .banner-modal-wrapper {
-//     padding: 20px 20px 0 20px;
-//     margin-bottom: 180px;
-//   }
+    .close-button {
+      border: 1px solid #000;
+      padding: 10px;
+      cursor: pointer;
+    }
 
-//   .banner-modal-content {
-//     margin-bottom: 40px;
+    @media (min-width: 600px) {
+      padding: 30px 30px 0 0;
+    }
+    @media (min-width: 960px) {
+      padding: 20px 20px 0 0;
+    }
+  }
 
-//     &:last-child {
-//       margin-bottom: 0;
-//     }
+  .banner-modal-content {
+    padding: 20px 20px 0 20px;
+    margin-bottom: 180px;
 
-//     h1 {
-//       font-size: 16px;
-//       font-weight: bold;
-//       line-height: 1.25;
-//       margin-bottom: 10px;
-//     }
+    .content {
+      margin-bottom: 40px;
 
-//     p {
-//       font-size: 14px;
-//       font-weight: normal;
-//       line-height: 1.4;
-//       margin-bottom: 15px;
-//     }
+      &:last-child {
+        margin-bottom: 0;
+      }
 
-//     a {
-//       display: inline-block;
-//       font-size: 13px;
-//       font-weight: bold;
-//       line-height: 1.3;
-//       letter-spacing: 0.15em;
-//       text-decoration: underline;
-//       transition: all 0.3s ease-in-out;
+      h1 {
+        font-size: 16px;
+        font-weight: 700;
+        line-height: 1.25;
+        text-transform: uppercase;
+        margin-bottom: 10px;
+      }
+      p {
+        font-size: 14px;
+        font-weight: 400;
+        line-height: 1.4;
+        margin-bottom: 15px;
+        max-height: 100px;
+        overflow: hidden;
+      }
+      a {
+        display: inline-block;
+        font-size: 13px;
+        font-weight: 700;
+        letter-spacing: 0.15em;
+        line-height: 1.3;
+        text-decoration: underline;
+        max-width: 300px;
+        flex: 1 1 auto;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        transition: all 0.3s ease-in-out;
 
-//       &:hover {
-//         color: #fff;
-//         background-color: #000;
-//       }
-//     }
-//   }
+        &:hover {
+          background-color: ${({ theme }) => theme.colors.black};
+          color: ${({ theme }) => theme.colors.white};
+        }
+      }
+    }
 
-//   @media (min-width: 600px) {
-//     height: 100%;
+    @media (min-width: 600px) {
+      padding: 126px 0 0 60px;
+      margin-bottom: 200px;
 
-//     .banner-modal-close-button-wrapper {
-//       padding: 30px 30px 0 0;
-//     }
+      .content {
+        width: 400px;
+        margin-bottom: 80px;
+      }
+    }
+    @media (min-width: 960px) {
+      display: flex;
+      justify-content: space-evenly;
+      align-items: flex-start;
+      padding: 30px 0 0 0;
+      margin-bottom: 100px;
 
-//     .banner-modal-wrapper {
-//       width: 100%;
-//       padding: 126px 0 0 60px;
-//       margin-bottom: 200px;
-    
-//       .banner-modal-content {
-//         width: 400px;
-//         margin-bottom: 80px;
-//       }
-//     }
-//   }
-//   @media (min-width: 960px) {
-//     max-height: 500px;
-//     height: auto;
+      .content {
+        width: 335px;
+        margin-bottom: 0;
+      }
+    }
+  }
+`;
 
-//     .banner-modal-close-button-wrapper {
-//       padding: 20px 20px 0 0;
-//     }
+const Overlay = styled.div`
+  display: block;
+  z-index: 999;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
 
-//     .banner-modal-wrapper {
-//       display: flex;
-//       flex-direction: row;
-//       justify-content: center;
-//       align-items: flex-start;
-//       padding: 30px 0 0 0;
-//       margin-bottom: 100px;
-
-//       .banner-modal-content {
-//         width: 335px;
-//         margin-right: 80px;
-//         margin-bottom: 0;
-
-//         &:last-child {
-//           margin-right: 0;
-//         }
-//       }
-//     }
-//   }
-// `;
-
-// const Overlay = styled.div`
-//   display: block;
-//   z-index: 999;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   width: 100%;
-//   height: 100%;
-//   background-color: rgba(0, 0, 0, 0.5);
-
-//   &.close {
-//     display: none;
-//   }
-// `;
+  &.modal-close {
+    display: none;
+  }
+`;
 
 const Banner = () => {
   const [banner] = useState(BannerData);
@@ -261,16 +231,35 @@ const Banner = () => {
   };
   
   return (
-    <Container id="banner" className="banner" role="banner" onClick={ bannerHandler }>
-      <BannerWrapper>
-        <h1 className="banner-title" aria-label="배너 내용">
-          { text }                
-        </h1>
-        <div className="banner-icon-more" aria-label="더보기 아이콘">
-          <IconMoreDown width={24} height={22} fill="#fff" />
+    <>
+      <Container id="banner" className="banner" role="banner" onClick={ bannerHandler }>
+        <BannerWrapper>
+          <h1 className="banner-title" aria-label="배너 내용">
+            { text }                
+          </h1>
+          <div className="banner-icon-more" aria-label="더보기 아이콘">
+            <IconMoreDown width={24} height={22} fill="#fff" />
+          </div>
+        </BannerWrapper>
+      </Container>
+      <BannerModal id="banner-modal" className={bannerToggle === true ? "modal-open" : null}>
+        <div className="banner-modal-close-button" onClick={ bannerHandler }  aria-label="모달 닫기 버튼" >
+          <IconCloseButton className="close-button" width={50} height={50} fill="#000" />
         </div>
-      </BannerWrapper>
-    </Container>
+        <div className="banner-modal-content">
+            {
+              banner.map((bannerData, i) => (
+                <div className="content" key={i}>
+                  <h1>{ bannerData.title }</h1>
+                  <p>{ bannerData.content }</p>
+                  <a href={ bannerData.url_link }>{ bannerData.url_title }</a>
+                </div>
+              ))
+            }
+        </div>
+      </BannerModal>
+      <Overlay id="overlay" className={bannerToggle === true ? null : "modal-close"} onClick={ bannerHandler } aria-hidden />
+    </>
   );
 }
 
