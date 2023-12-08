9cda08105cbf1d55ffde28fba22c88840a74b417
kimhyesom <kimhyesom1202@gmail.com>
2023-12-08 15:45:17 +0900

    💄 Setting Global styles

---
+++ src/GlobalStyle.js
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  /* 전역 변수 지정 */
+`;
+
+export default GlobalStyle;
---
+++ src/Theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  // ${({ theme }) => theme.colors.컬러이름};
+  colors: {
+    black: '#000',
+    white: '#fff',
+  },
+
+  // ${({ theme }) => theme.fs.size};
+  fs: {
+    10: '10px',
+    11: '11px',
+    12: '12px',
+  },
+
+  // ${({ theme }) => theme.margin.size};
+  margin: {
+    10: '10px',
+  }
+}
+
+export default theme;
