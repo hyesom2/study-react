27914a6335adae34389cfbeedfb3af1a5acaf7a3
kimhyesom <kimhyesom1202@gmail.com>
2023-12-14 15:53:08 +0900

    🎨 Add id of SideMenuData

--- src/assets/data/SideMenuData.js
+++ src/assets/data/SideMenuData.js
@@ -1,30 +1,37 @@
 const SideMenuTop = [
   {
+    id: 0,
     name: 'men',
     des: 'men 카테고리 열기 버튼',
   },
   {
+    id: 1,
     name: 'women',
     des: 'women 카테고리 열기 버튼'
   },
   {
+    id: 2,
     name: 'kids',
     des: 'kids 카테고리 열기 버튼',
   },
   {
+    id: 3,
     name: 'sports',
     des: 'sports 카테고리 열기 버튼',
   },
   {
+    id: 4,
     name: 'brands',
     des: 'brands 카테고리 열기 버튼',
   },
   {
+    id: 5,
     name: 'members exclusives',
     des: 'members exclusives 페이지 이동 버튼',
     link: '/'
   },
   {
+    id: 6,
     name: 'online outlet',
     des: 'online outlet 페이지 이동 버튼',
   },
@@ -32,45 +39,53 @@ const SideMenuTop = [
 
 const SideMenuBottom = [
   {
+    id: 0,
     name: '주문조회',
     link: '/',
     des: '주문조회 페이지 이동'
   },
   {
+    id: 1,
     name: '로그인 하기',
     link: '/',
     des: '로그인 페이지 이동'
   },
   {
+    id: 2,
     name: '아디클럽 가입하기',
     link: '/',
     des: '아디클럽 가입 페이지 이동'
   },
   {
+    id: 3,
     name: '도움말',
     link: '/',
     des: '도움말 페이지 이동'
   },
   {
+    id: 4,
     name: '반품',
     link: '/',
     des: '반품 페이지 이동'
   },
   {
+    id: 5,
     name: 'adidas Runners',
     link: '/',
     des: 'adidas Runners 페이지 이동'
   },
   {
+    id: 6,
     name: 'EARN POINTS. GET REWARDS',
     link: '/',
     des: 'earn points, get rewards 페이지 이동'
   },
   {
+    id: 7,
     name: '피드백',
     link: '/',
     des: '피드백 페이지 이동'
-  },
+  }
 ];
 
 export {SideMenuTop, SideMenuBottom};
