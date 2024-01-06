4e7536ab2c798bd45876fadd4f25113b2e92c390
kimhyesom <kimhyesom1202@gmail.com>
2024-01-06 15:38:24 +0900

    ✨ Pass example data to each components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Poster from './components/Main/Poster/Poster';
 import Product from './components/Main/Product/Product';
+import ProductList from './components/Main/Product/ProductList';
 import Carousel from './components/Main/Carousel/Carousel';
 import Explanation from './components/Main/Explanation';
+import BlogCarousel from './components/Main/Carousel/BlogCarousel';
 import Footer from './components/Footer';
 // > data
-import { ProductDataTrend, ProductDataHoodies }from './assets/data/ProductData';
+import { ProductDataTrend, ProductDataHoodies, ProductDataOuter, ProductDataAcc, ProductDataBest }from './assets/data/ProductData';
+import { ProductBlogData, StoryBlogData } from './assets/data/BlogData';
+
 
 const App = () => {
   return (
@@ -17,9 +21,9 @@ const App = () => {
       <Main>
         <Poster />
         <Product ProductDataTrend={ ProductDataTrend } ProductDataHoodies={ ProductDataHoodies } />
-        <Carousel />
-        <Product />
-        <Carousel />
+        <Carousel ProductBlogData={ ProductBlogData } />
+        <ProductList ProductDataOuter={ ProductDataOuter } ProductDataAcc={ ProductDataAcc } ProductDataBest={ ProductDataBest }/>
+        <BlogCarousel StoryBlogData={ StoryBlogData } />
         <Explanation />
       </Main>
       <Footer />
